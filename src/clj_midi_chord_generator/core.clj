a16cd65686ed67509898037411247183bf50f3a9
Harold <hhausman@gmail.com>
2019-12-14 17:14:21 -0700

    [chords] Toward more chords. Still some known naming problems, and likely some other unknown problems.

--- src/clj_midi_chord_generator/core.clj
+++ src/clj_midi_chord_generator/core.clj
@@ -5,13 +5,54 @@
            [javax.sound.midi Sequence ShortMessage MidiEvent MidiSystem])
   (:gen-class))
 
-(def version 0)
+(def version "0.1")
 
-(def qualities [{:name "Maj" :intervals [0 4 7]}
-                {:name "min" :intervals [0 3 7]}
-                {:name "dim" :intervals [0 3 6]}
-                {:name "aug" :intervals [0 4 8]}
-                {:name "sus4" :intervals [0 5 7]}])
+(def qualities [{:name " major" :intervals [0 4 7]}
+                {:name " dim" :intervals [0 3 6]}
+                {:name " augmented" :intervals [0 4 8]}
+                {:name "sus4" :intervals [0 5 7]}
+                {:name "6" :intervals [0 4 7 9]}
+                {:name "7" :intervals [0 4 7 10]}
+                {:name "m7(b5)" :intervals [0 3 6 10]}
+                {:name "dim7" :intervals [0 3 6 9]}
+                {:name "7b9" :intervals [0 4 7 10 13]}
+                {:name "7#9" :intervals [0 4 7 10 15]}
+                {:name "9" :intervals [0 4 7 10 14]}
+                {:name "9sus4" :intervals [0 5 7 10 14]}
+                {:name "9b5" :intervals [0 4 6 10 14]}
+                {:name "9#5" :intervals [0 4 8 10 14]}
+                {:name "9#11" :intervals [0 4 7 10 18]}
+                {:name "13" :intervals [0 4 7 10 14 21]}
+                {:name "13sus4" :intervals [0 5 7 10 14 21]}
+                {:name "13b5" :intervals [0 4 6 10 14 21]}
+                {:name "13#5" :intervals [0 4 8 10 14 21]}
+                {:name "13b9" :intervals [0 4 7 10 13 21]}
+                {:name "13#9" :intervals [0 4 7 10 15 21]}
+                {:name "13b5b9" :intervals [0 4 6 10 13 21]}
+                {:name "13b5#9" :intervals [0 4 6 10 15 21]}
+                {:name "13#5b9" :intervals [0 4 8 10 13 21]}
+                {:name "13#5#9" :intervals [0 4 8 10 15 21]}
+                {:name " major7" :intervals [0 4 7 11]}
+                {:name " 6-9" :intervals [0 4 7 9 14]}
+                {:name " major9" :intervals [0 4 7 11 14]}
+                {:name " major9#11" :intervals [0 4 7 11 14 18]}
+                {:name " major13" :intervals [0 4 7 11 14 21]}
+                {:name " major13b5" :intervals [0 4 6 11 14 21]}
+                {:name " major13#5" :intervals [0 4 8 11 14 21]}
+                {:name " major13b9" :intervals [0 4 7 11 13 21]}
+                {:name " major13#9" :intervals [0 4 7 11 15 21]}
+                {:name " major13b5b9" :intervals [0 4 6 11 13 21]}
+                {:name " major13b5#9" :intervals [0 4 6 11 15 21]}
+                {:name " major13#5b9" :intervals [0 4 8 11 13 21]}
+                {:name " major13#5#9" :intervals [0 4 8 11 15 21]}
+                {:name " minor6" :intervals [0 3 7 9]}
+                {:name " minor7" :intervals [0 3 7 10]}
+                {:name " minor7b9" :intervals [0 3 7 10 13]}
+                {:name " minor9" :intervals [0 3 7 10 14]}
+                {:name " minor11" :intervals [0 3 7 10 14 17]}
+                {:name " minor13" :intervals [0 3 7 10 14 21]}
+                {:name " minor (major7)" :intervals [0 3 7 11]}
+                {:name " minor9 (major7)" :intervals [0 3 7 11 14]}])
 
 (def roots [{:name "C" :offset 0}
             {:name "C# (Db)" :offset 1}
@@ -27,19 +68,26 @@
             {:name "B" :offset 11}])
 
 (def scales [{:name "Major" :intervals [0 2 4 5 7 9 11]}
-             {:name "minor" :intervals [0 2 3 5 7 8 10]}])
+             {:name "minor" :intervals [0 2 3 5 7 8 10]}
+             {:name "harmonic minor" :intervals [0 2 3 5 7 8 11]}])
+
+(defn- chord-instances
+  [root quality]
+  (loop [out []
+         notes (vec (map #(+ (:offset root) %) (:intervals quality)))]
+    (if (every? #(<= 0 % 127) notes)
+      (recur (conj out notes)
+             (if (> 12 (-> quality :intervals last))
+               (vec (sort (concat [(+ 12 (nth notes 0))]
+                                  (rest notes))))
+               (mapv #(+ 12 %) notes)))
+      out)))
 
 (defn- chord-near-middle-c
   [root quality]
-  (let [instances (loop [out []
-                         notes (vec (map #(+ (:offset root) %) (:intervals quality)))]
-                    (if (every? #(<= 0 % 127) notes)
-                      (recur (conj out notes)
-                             (vec (sort (concat [(+ 12 (nth notes 0))]
-                                                (rest notes)))))
-                      out))
+  (let [instances (chord-instances root quality)
         distance (fn [notes] (apply + (map #(Math/abs (- % 60)) notes)))]
-    {:name (format "%s %s" (:name root) (:name quality))
+    {:name (format "%s%s" (:name root) (:name quality))
      :offset (:offset root)
      :notes (apply min-key distance instances)}))
 
@@ -73,15 +121,16 @@
         base-path (format "./midi-chords-v%s" version)]
     (shell/sh "rm" "-rf" base-path)
     (doseq [root roots
-            scale scales]
+            [scale-index scale] (map-indexed vector scales)]
       (let [note-set (root-scale->note-set root scale)]
         (doseq [chord (filter #(every? note-set (:notes %)) chords)]
           (let [sequence (Sequence. Sequence/PPQ 24)
                 track (.createTrack sequence)
                 scale-degree (inc (.indexOf (:intervals scale)
                                             (mod (- (:offset chord) (:offset root)) 12)))
-                path (format "%s/%02d - %s %s/%02d - %s.mid" base-path
-                             (:offset root) (:name root) (:name scale) scale-degree (:name chord))]
+                path (format "%s/%02d.%s - %s %s/%02d - %s.mid" base-path
+                             (:offset root) scale-index
+                             (:name root) (:name scale) scale-degree (:name chord))]
             (doseq [note (:notes chord)]
               (add-note-to-track! track note))
             (io/make-parents path)
